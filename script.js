2fd721a8794b022c927f814c550e299ec6ef648b
av547302-coder <av547302@gmail.com>
2026-01-29 19:56:32 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 400;
+    let height = sec.offsetHeight;
+
+    if (top >= offset && top < offset + height) {
+      sec.classList.add("show");
+    }
+  });
+});
